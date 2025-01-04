87ed9e821e57ec84aba7e4cb7660ef2d0f86fca6
bamjun <khy5116@naver.com>
2025-01-04 16:31:47 +0900

    feat: Add new function for enhanced lottery data processing and update message formatting
    - Introduced a new function `a_test_3` for processing lottery data and logging formatted messages.
    - Updated `scrapeLottoDataToDiscord` to include additional checks for data validity before message creation.
    - Enhanced message formatting with links to raw data and spreadsheet, improving clarity and accessibility of prize details.

--- A_test.js
+++ A_test.js
@@ -17,6 +17,23 @@ function a_test_2() {
 }
 
 
+function a_test_3() {
+  var rawData = getTrippleLuckRawData();
+  const jsonData = JSON.parse(convertWinnersListDataJson(rawData));
+  saveDataToSpreadsheet(jsonData, rawData)
+
+  var lastRowData = getLastRow({row: 1})[0];
+  
+  if (parseInt(lastRowData[9]) == 0 || parseInt(lastRowData[9]) % 2 != 0) {
+    return;
+  }
+  
+  var content_text = `\`${lastRowData[4]} - ${lastRowData[5]}\` [❇️링크](${getTrippleLuckRawData_URL})  [❇️시트](${url_spreadsheet_tripleLuckData})\n \`10만원 - ${lastRowData[6]}번\`\n \`100만원 - ${lastRowData[7]}번\`\n \`500만원 - ${lastRowData[8]}번\`\n \`1억원 - ${lastRowData[9]}번\`\n \`5억원 - ${lastRowData[10]}번\`\n`;
+  content_text += "\`\`\`\n \n\`\`\`" + convertWinnersListData(rawData);
+  Logger.log(content_text);
+}
+
+
 
 
 // Logger.log(getLastRow({row: 8}))
--- Code.js
+++ Code.js
@@ -14,8 +14,16 @@ function scrapeLottoDataToDiscord() {
   saveDataToSpreadsheet(jsonData, rawData)
 
   var lastRowData = getLastRow({row: 1})[0];
-  var content_text = `\`${lastRowData[4]} - ${lastRowData[5]}\` [링크](${getTrippleLuckRawData_URL})\n \`10만원 - ${lastRowData[6]}번\`\n \`100만원 - ${lastRowData[7]}번\`\n \`500만원 - ${lastRowData[8]}번\`\n \`1억원 - ${lastRowData[9]}번\`\n \`5억원 - ${lastRowData[10]}번\`\n\n`;
-  content_text += "\n\n" + convertWinnersListData(rawData);
+  
+  if (parseInt(lastRowData[9]) == 0 || parseInt(lastRowData[9]) % 2 != 0) {
+    return;
+  }
+  if ((parseInt(lastRowData[9]) == 2 && parseInt(lastRowData[10]) == 1) || (parseInt(lastRowData[9]) == 4 && parseInt(lastRowData[10]) == 2) || (parseInt(lastRowData[9]) == 6 && parseInt(lastRowData[10]) == 3) || (parseInt(lastRowData[9]) == 8 && parseInt(lastRowData[10]) == 4)) {
+    return;
+  }
+  
+  var content_text = `\`${lastRowData[4]} - ${lastRowData[5]}\` [❇️링크](${getTrippleLuckRawData_URL})  [❇️시트](${url_spreadsheet_tripleLuckData})\n \`10만원 - ${lastRowData[6]}번\`\n \`100만원 - ${lastRowData[7]}번\`\n \`500만원 - ${lastRowData[8]}번\`\n \`1억원 - ${lastRowData[9]}번\`\n \`5억원 - ${lastRowData[10]}번\`\n`;
+  content_text += "\`\`\`\n \n\`\`\`" + convertWinnersListData(rawData);
   sendToDiscord(content_text);
 }
 
